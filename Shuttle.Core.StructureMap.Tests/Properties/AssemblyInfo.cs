0e8a8fa7caccddc8f51e7a6b486ea2d872f9ad6a
Eben <me@ebenroux.co.za>
2017-02-26 21:12:56 +0200

    - 2017

--- Shuttle.Core.StructureMap.Tests/Properties/AssemblyInfo.cs
+++ Shuttle.Core.StructureMap.Tests/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.Core.StructureMap.Tests")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyCopyright("Copyright © Eben Roux 2016")]
+[assembly: AssemblyCopyright("Copyright © Eben Roux 2017")]
 [assembly: AssemblyProduct("Shuttle.Core")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
