bd2a2edc9d21b39c29b9625ccbf1080c1d524b59
eben-roux <me@ebenroux.co.za>
2018-01-01 20:22:06 +0200

    - .net core refactoring

---
+++ Shuttle.Core.StructureMap.Tests/Fixture.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using Shuttle.Core.Container.Tests;
+using StructureMap;
+
+namespace Shuttle.Core.StructureMap.Tests
+{
+    [TestFixture]
+    public class StructureMapComponentContainerFixture : ContainerFixture
+    {
+        [Test]
+        public void Should_be_able_resolve_all_instances()
+        {
+            var containerBuilder = new Registry();
+
+            var registry = new StructureMapComponentRegistry(containerBuilder);
+
+            RegisterCollection(registry);
+
+            var resolver = new StructureMapComponentResolver(new global::StructureMap.Container(containerBuilder));
+
+            ResolveCollection(resolver);
+        }
+
+        [Test]
+        public void Should_be_able_to_register_and_resolve_a_singleton()
+        {
+            var containerBuilder = new Registry();
+
+            var registry = new StructureMapComponentRegistry(containerBuilder);
+
+            RegisterSingleton(registry);
+
+            var resolver = new StructureMapComponentResolver(new global::StructureMap.Container(containerBuilder));
+
+            ResolveSingleton(resolver);
+        }
+
+        [Test]
+        public void Should_be_able_to_register_and_resolve_transient_components()
+        {
+            var containerBuilder = new Registry();
+
+            var registry = new StructureMapComponentRegistry(containerBuilder);
+
+            RegisterTransient(registry);
+
+            var resolver = new StructureMapComponentResolver(new global::StructureMap.Container(containerBuilder));
+
+            ResolveTransient(resolver);
+        }
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_a_multiple_singleton()
+		{
+			var containerBuilder = new Registry();
+
+			var registry = new StructureMapComponentRegistry(containerBuilder);
+
+			RegisterMultipleSingleton(registry);
+
+			var resolver = new StructureMapComponentResolver(new global::StructureMap.Container(containerBuilder));
+
+			ResolveMultipleSingleton(resolver);
+		}
+
+		[Test]
+		public void Should_be_able_to_register_and_resolve_multiple_transient_components()
+		{
+			var containerBuilder = new Registry();
+
+			var registry = new StructureMapComponentRegistry(containerBuilder);
+
+			RegisterMultipleTransient(registry);
+
+			var resolver = new StructureMapComponentResolver(new global::StructureMap.Container(containerBuilder));
+
+			ResolveMultipleTransient(resolver);
+		}
+	}
+}
